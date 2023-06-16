874dd82391373c522ea439667719b7f66a557455
rasheed <rasheedjawula18@gmail.com>
2023-06-15 21:26:06 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point for the program.
+ * It prints a message to the console.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
 
