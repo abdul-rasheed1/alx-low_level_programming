a1fcb60e8f62df638d82307fe0e0f475acdde073
rasheed <rasheedjawula18@gmail.com>
2023-06-15 20:48:16 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function is the entry point for the program.
+ * It prints a message to the console.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+
+return (0);
+
+
+}
