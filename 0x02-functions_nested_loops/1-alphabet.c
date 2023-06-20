e9d18b1772d2e0fca5ce5cc4590dcf3a454bbdcc
rasheed <rasheedjawula18@gmail.com>
2023-06-20 10:15:56 -0700

    function that prints alpahbets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * print_alphabet: prints lowercase aphabets
+ * Return: Always 0
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		int a;
+
+		a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+		}
+		putchar('\n');
+		return (0);
+	}
+	return (0);
+}
