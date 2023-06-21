bedf53c7fccebf5ffb5063cff378d84fb9f7865c
rasheed <rasheedjawula18@gmail.com>
2023-06-20 23:02:26 -0700

    last digs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,13 @@ int print_last_digit(int n)
 {
 	int last;
 
-	 if (n < 0)
-	 {
-		 n = -n;
-	 }
-
-
-	last = (n % 10);
+	if (n < 0)
+	{
+		n = -n;
+	}
+	last = n % 10;
 	_putchar(last + '0');
 	return (last);
+
 }
 
