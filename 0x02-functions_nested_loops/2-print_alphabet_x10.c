faa1c0fabc19deee7f90eb479371a93f0ae39da8
rasheed <rasheedjawula18@gmail.com>
2023-06-20 17:56:18 -0700

    10x alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,13 @@
 /**
  * print_alphabets_x10 - prints alphabets x10 times
  *
- * Description: prints x10 all alphabets
+ * Description prints x10 all alphabets
  */
 void print_alphabet_x10(void)
 {
 	int i, a;
 
-	for (i = '1'; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
