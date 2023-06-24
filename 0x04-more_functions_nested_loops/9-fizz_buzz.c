8d9807e022770aa4ea0391bfc931cca68b53a1f4
rasheed <rasheedjawula18@gmail.com>
2023-06-23 22:40:50 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if (c % 3 == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+		else if ( c % 3 == 0 && c % 5 == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", c);
+			putchar(' ');
+		}
+	}
