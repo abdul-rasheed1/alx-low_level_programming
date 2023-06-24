b8b9cc11f4e276e6bee1b523964f59b13a2daefb
rasheed <rasheedjawula18@gmail.com>
2023-06-23 18:05:28 -0700

    show 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
