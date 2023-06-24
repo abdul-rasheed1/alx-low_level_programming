6f5eb0e20aa5701c23e275ea7e084ee60625fd23
rasheed <rasheedjawula18@gmail.com>
2023-06-23 21:00:41 -0700

    diagonal liine

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: length of diagonal
+ *
+ * Return: nothinf
+ */
+void print_diagonal(int n)
+{
+	int l, s;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (l = 1; l <= n; l++)
+		{
+			for (s = 0; s < l; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+			_putchar('\n');
+		}
+
+	}
+}
