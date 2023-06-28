3a05523a1f53f8ff15a5d01a789c3db0283089cb
rasheed <rasheedjawula18@gmail.com>
2023-06-27 17:34:10 -0700

    array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,13 +13,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n - 1; i++)
 	{
-
-	printf("%d, ", a[i]);
+		if (i == n - 1)
+		{
+			printf("%d\n", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
 	}
-
-if (i == n - 1)
-	{
-	printf("%d\n", a[n - 1]);
-
-	}
-}
