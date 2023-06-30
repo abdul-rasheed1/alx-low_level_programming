62961b8233f89451f287866d46713172070fac82
rasheed <rasheedjawula18@gmail.com>
2023-06-30 16:42:09 -0700

    string copy wth specified numbers of chars

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - copiees string with specified number of characters
+ * @dest: the new copied strng
+ * @src: source of the string
+ * @n: specific number of chars to be copied
+ *
+ * Return: the copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *p;
+
+	p = dest;
+	while (*src != '\0' && n != 0)
+	{
+		*p = *src;
+		p++;
+		src++;
+		n--;
+	}
+	*p = '\0';
+	return (dest);
+}
