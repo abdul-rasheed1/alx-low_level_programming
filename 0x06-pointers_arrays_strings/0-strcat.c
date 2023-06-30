c0c9595b31d7bf4fec41ada799dba8c1029305dd
rasheed <rasheedjawula18@gmail.com>
2023-06-30 15:15:08 -0700

    strcat debugged

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,19 +7,20 @@
  * Return: the concatenated string
  */
 char *_strcat(char *dest, char *src)
-{       
-        char *p;
-        
-        p = dest;
-        while (*p != '\0')
-        {       
-                p++;
-        }       
-        while (*src != '\0')
-        {       
-                *p = *src;
-                p++;
-                src++;
-        }
-	*p = '\0';
-	return (dest);
+{
+char *p;
+
+p = dest;
+while (*p != '\0')
+{
+p++;
+}
+while (*src != '\0')
+{
+*p = *src;
+p++;
+src++;
+}
+*p = '\0';
+return (dest);
+}
