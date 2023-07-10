a1557433416369d52b99f0c21808a42c0d665859
rasheed <rasheedjawula18@gmail.com>
2023-07-09 20:02:16 -0700

    prints name of the program which is its first arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector containing strings of the args
+ *
+ * Return: 0 always succes
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
