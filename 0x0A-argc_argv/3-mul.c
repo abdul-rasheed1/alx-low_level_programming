8d529693984113ec71798ad8afef422b7e7f8348
rasheed <rasheedjawula18@gmail.com>
2023-07-09 21:28:22 -0700

    product of two numbers using argv and argc

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: number of args
+ * @argv: the strings of the args
+ *
+ * Return: 0 for success 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, p;
+
+	n1 = 0;
+	n2 = 0;
+	p = 0;
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		p = n1 * n2;
+		printf("%d\n", p);
+	}
+	return (0);
+}
