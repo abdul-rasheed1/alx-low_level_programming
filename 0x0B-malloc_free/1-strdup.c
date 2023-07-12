b5e18f452ba13172ea86b3d75fb4b6f0c50cf37b
rasheed <rasheedjawula18@gmail.com>
2023-07-11 20:57:38 -0700

    task two of malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _stdrup - duplicates the string str
+ * @str: the string to be copied
+ *
+ * Return: a pointer to a chasr
+ */
+char *_strdup(char *str)
+{
+	int i, n;
+	char *w;
+
+	i = 0;
+	n = 0;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i])
+	{
+		i++;
+	}
+	w = malloc((sizeof(char) * i) + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	while (n < i)
+	{
+		w[n] = str[n];
+		n++;
+	}
+	w[n] = '\0';
+	return (w);
+}
