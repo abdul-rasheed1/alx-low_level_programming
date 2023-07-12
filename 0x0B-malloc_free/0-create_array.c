396778d14efd023eecc3eb16227822de1613e01f
rasheed <rasheedjawula18@gmail.com>
2023-07-11 17:41:43 -0700

    malloc code debug

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	ar = (char *)malloc(size * sizeof(char));
+	ar = malloc(size * sizeof(char));
 	if (ar == NULL)
 	{
 		return (NULL);
